151d9a65339a7816868a4451053d33a58750ccd1
cloconut <campbellc@macstudent.com.au>
2025-05-15 09:42:42 +0000

    ratebook method

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-# Write Python code for both the Novel and Magazine classes modelled in the previous slide. Include a suitable constructor method which uses the Book constructor method. Instantiate 2 novels and 2 magazines and print their details.
-# Create the Book class (plus methods and attributes)
-# Create the Novel class that inherits from Book class.
-# Create the Magazine class that inherits from Book class.
-
 # BOOK CLASS (PARENT)
 class Book():
     def __init__(self, title, author, year, numpages):
@@ -11,29 +6,30 @@ class Book():
         self.year = year
         self.numpages = numpages
 
-    # Methods
+  # Methods
     def displaydetails(self):
-        print(f'Title:{title}')
-        print(f'Author:{author}')
-        print(f'Year of Publication:{year}')
-        print(f'Number of Pages:{numpages}')
-
+        print(f'Title: {self.title}')
+        print(f'Author: {self.author}')
+        print(f'Year of Publication: {self.year}')
+        print(f'Number of Pages: {self.numpages}')
 
     def ratebook(self):
-        rate = input('What will you rate this book? ★★★★★ (Enter 1-5 for rating)')
-        if rate == 1:
-            print(f'You have rated {title} 1 star! What a shame...')
-        if rate == 2:
-            print(f'You have rated {title} 2 stars! Not your cup of tea?')
-        if rate == 3:
-            print(f'You have rated {title} 3 stars! Cool beans.')
-        if rate == 4:
-            print(f'You have rated {title} 4 stars! Sounds like a good read!')
-        if rate == 5:
-            print(f'You have rated {title} 5 stars! Amazing!!')
+        rate = input('What will you rate this book? ★★★★★ (Enter 1-5 for rating) ')
+        if rate == '1':
+            print(f'You have rated {self.title} 1 star! What a shame...')
+        if rate == '2':
+            print(f'You have rated {self.title} 2 stars! Not your cup of tea?')
+        if rate == '3':
+            print(f'You have rated {self.title} 3 stars! Cool beans.')
+        if rate == '4':
+            print(f'You have rated {self.title} 4 stars! Sounds like a good read!')
+        if rate == '5':
+            print(f'You have rated {self.title} 5 stars! Amazing!!')
         else:
             print("Sorry, that rating isn't valid. Please resubmit your rating")
-            ratebook(self)
+            Book.ratebook(self)
+
+# - - - - - - - - - - - - - - #
 
 # NOVEL CLASS
 class Novel(Book):
@@ -42,13 +38,17 @@ class Novel(Book):
             self.genre = genre
             self.numchapters = numchapters
 
-    # Methods
+  # Methods
     def calcreadtime(self):
-        wpm = ('How many words per average can you read?')
-        readtime = 275 / wpm * numpages
+        wpm = int(input('How many words per minute can you read? '))
+        averagewords = int(self.numpages) * 275  # Assuming 275 words per page
+        readtime = averagewords / wpm
+        print(f"Estimated reading time: {readtime:.2f} minutes")
 
 americanpsycho = Novel('American Psycho', 'Bret Easton Ellis', '1991', '400', 'Psychological Horror / Dark Comedy', '60')
 nt84 = Novel('1894', 'George Orwell', '1949', '336', 'Science Fiction / Dystopian', '23')
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
 
 # MAGAZINE CLASS
 class Magazine(Book):
@@ -57,37 +57,70 @@ class Magazine(Book):
         self.issurenum = issuenum
         self.numarticles = numarticles
 
-    # Methods
+  # Methods
 #get article by title
+
+# - - - - - - - - - - - #
 
 # PROGRAM
 def novellist():
     print('NOVELS:')
-    print('  American Psycho')
-    print('  1894')
+    print(' - American Psycho')
+    print(' - 1984')
 
 def magazinelist():
     print('MAGAZINES:')
     print('eugh')
 
+print('Welcome to the most underwhelming library to exist.')
+
 def main():  
-    print('Welcome to the most underwhelming library to exist.')
     typereq = input('What type of books were you after? (Novel / Magazine) ').lower()
 
+# Novels
     if typereq == 'novel':
         print('What novel would you like to inquire about? ')
         novellist()
         novchoice = input('Enter: ')
 
-        if novchoice == 'american psycho':
-            print('What would you like to know about American Psycho?')
-            print(' 1. Details')
-            print(' 2. Reading Time')
+  # American Psycho
+    if novchoice == 'american psycho':
+        print('What would you like to know about American Psycho?')
+        print(' 1. Details')
+        print(' 2. Reading Time')
+        actionnov = input('Enter: ').lower()
 
-            actionnov = input('Enter: ').lower()
+        if actionnov == '1' or actionnov == 'details':
+            americanpsycho.displaydetails()
+        if actionnov == '2' or actionnov == 'reading time':
+            americanpsycho.calcreadtime()
+        
+        willrate = input('Would you like to rate this novel? (yes/no) ').lower()
+        if willrate == 'yes':
+            americanspycho.ratebook()
+        if willrate == 'no':
+            print('Okay!')
+            main()       
 
-            if actionnov == '1' or 'details':
-                print(displaydetails.(americanpsycho))
+  # 1984
+    if novchoice == '1984':
+        print('What would you like to know about 1984?')
+        print(' 1. Details')
+        print(' 2. Reading Time')
+        actionnov = input('Enter: ').lower()
+
+        if actionnov == '1' or actionnov == 'details':
+            nt84.displaydetails()
+        if actionnov == '2' or actionnov == 'reading time':
+            nt84.calcreadtime()
+
+        willrate = input('Would you like to rate this novel? (yes/no) ').lower()
+        
+        if willrate == 'yes':
+            nt84.ratebook()
+        if willrate == 'no':
+            print('Okay!')
+            main()
 
 main()
 
